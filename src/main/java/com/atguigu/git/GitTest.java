ba613e5b908eba105c160fb153c6b28ffb77de71
root <123@qq.com>
2021-11-13 21:20:57 +0800

    first-idea commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author YJP
+ * @create 2021-11-13 21:17
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
